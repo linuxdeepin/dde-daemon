83c7c0a3e8a806739c787efe9bee513cb56872ba
zhangxiangsong <zhangxiangsong@uniontech.com>
2020-06-11 16:28:00 +0800

    fix: 根据SP2需求修改卸载应用后的通知内容
    根据SP2需求，卸载应用通知内容不再包含应用的名称，直接提示卸载成功或者卸载失败。
    
    Log: 简化卸载应用的通知内容，去掉具体的应用名称，只提示卸载成功或者卸载失败
    Bug: https://pms.uniontech.com/zentao/bug-view-31615.html
    Change-Id: I01af4061e37f7e74a294cc63440eb01f619582ac
    Reviewed-on: http://gerrit.uniontech.com/c/dde-daemon/+/2371
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: huangjiawei <huangjiawei@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- misc/ts/com.deepin.daemon.Fprintd.policy/policy_fi.ts
+++ misc/ts/com.deepin.daemon.Fprintd.policy/policy_fi.ts
@@ -21,5 +21,25 @@
 			<source>Clear fingerprint passwords</source>
 			<translation>Tyhjennä sormenjäljet</translation>
 		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!message" line="0"/>
+			<source>Authentication is required to rename fingerprint password</source>
+			<translation>Nimeä sormenjälki uudelleen, tämä vaatii tunnistautumisen</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!description" line="0"/>
+			<source>Rename fingerprint password</source>
+			<translation>Nimeä sormenjäljen salasana uudelleen</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.manager!message" line="0"/>
+			<source>Password is required to perform this action</source>
+			<translation>Tämän suorittamiseen tarvitaan salasana</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.manager!description" line="0"/>
+			<source>Manage fingerprint passwords</source>
+			<translation>Hallitse sormenjälkien tunnuksia</translation>
+		</message>
 	</context>
 </TS>
