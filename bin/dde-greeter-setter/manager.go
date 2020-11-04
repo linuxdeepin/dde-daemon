a94985383e070d0b07e547fcba68a7fbce92e537
hubenchang0515 <hubenchang@uniontech.com>
2020-11-04 16:27:22 +0800

    fix(dde-greeter-setter): use xsettingsd to config scale
    使用DTK的接口配置缩放,在没有插显示器的情况下,greeter会出现显示,改用xsettings来设置缩放
    
    Log: 修复缩放后重启时不插显示器,缩放错误的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-37775.html
    Change-Id: I2e9b66fc0652bd7cf657ebd425abdeb32cf52a02

--- bin/dde-greeter-setter/manager.go
+++ bin/dde-greeter-setter/manager.go
@@ -20,11 +20,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/godbus/dbus"
 	"pkg.deepin.io/lib/dbusutil"
+	"pkg.deepin.io/lib/keyfile"
 )
 
 const (
@@ -32,6 +36,34 @@ const (
 	dbusPath        = "/com/deepin/daemon/Greeter"
 	dbusInterface   = dbusServiceName
 )
+
+const (
+	themeFile                  = "/etc/lightdm/deepin/qt-theme.ini"
+	themeSection               = "Theme"
+	themeKeyIconThemeName      = "IconThemeName"
+	themeKeyFontSize           = "FontSize"
+	themeKeyFont               = "Font"
+	themeKeyMonoFont           = "MonoFont"
+	themeKeyScreenScaleFactors = "ScreenScaleFactors"
+	themeKeyScaleLogicalDpi    = "ScaleLogicalDpi"
+)
+
+const (
+	xsettingsFile             = "/etc/lightdm/deepin/xsettingsd.conf"
+	xsettingsKeyIconThemeName = "Net/IconThemeName"
+	xsettingsKeyFontSize      = "Qt/FontPointSize"
+	xsettingsKeyFont          = "Qt/FontName"
+	xsettingsKeyMonoFont      = "Qt/MonoFontName"
+	xsettingsKeyDpi           = "Xft/DPI"
+)
+
+// theme的key
+var globalKeyConvertMap = map[string]string{
+	themeKeyIconThemeName: xsettingsKeyIconThemeName,
+	themeKeyFontSize:      xsettingsKeyFontSize,
+	themeKeyFont:          xsettingsKeyFont,
+	themeKeyMonoFont:      xsettingsKeyMonoFont,
+}
 
 type Manager struct {
 	service *dbusutil.Service
@@ -47,6 +79,7 @@ func (m *Manager) UpdateGreeterQtTheme(fd dbus.UnixFD) *dbus.Error {
 	if err != nil {
 		logger.Warning(err)
 	}
+	updateXSettingsConfig()
 	return dbusutil.ToError(err)
 }
 
@@ -57,10 +90,7 @@ func updateGreeterQtTheme(fd dbus.UnixFD) error {
 	if err != nil {
 		return err
 	}
-	const (
-		themeFile     = "/etc/lightdm/deepin/qt-theme.ini"
-		themeFileTemp = themeFile + ".tmp"
-	)
+	const themeFileTemp = themeFile + ".tmp"
 	dest, err := os.Create(themeFileTemp)
 	if err != nil {
 		return err
@@ -85,6 +115,71 @@ func updateGreeterQtTheme(fd dbus.UnixFD) error {
 	return err
 }
 
+func updateXSettingsConfig() {
+	kf := keyfile.NewKeyFile()
+	err := kf.LoadFromFile(themeFile)
+	if err != nil {
+		logger.Warning(err)
+		return
+	}
+
+	xf, err := os.OpenFile(xsettingsFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		logger.Warning(err)
+		return
+	}
+	defer xf.Close()
+
+	// 将theme的配置转换成xsettings的配置
+	for themeKey, xsettingsKey := range globalKeyConvertMap {
+		value, err := kf.GetString(themeSection, themeKey)
+		if err == nil {
+			if isInteger(value) {
+				_, err = xf.WriteString(fmt.Sprintf("%s %s\n", xsettingsKey, value))
+			} else {
+				_, err = xf.WriteString(fmt.Sprintf("%s \"%s\"\n", xsettingsKey, value))
+			}
+		}
+
+		if err != nil {
+			logger.Warning(err)
+		}
+	}
+
+	// 换算缩放 xsettingsKeyDpi = themeKeyScreenScaleFactors * themeKeyScaleLogicalDpi * 1024
+	themeDpiStr, err := kf.GetString(themeSection, themeKeyScaleLogicalDpi)
+	if err != nil {
+		logger.Warningf("cannot get DPI: %v", err)
+		return
+	}
+	exp, err := regexp.Compile(`^([0-9]+),([0-9]+)$`)
+	if err != nil {
+		logger.Warning(err)
+		return
+	}
+	substr := exp.FindStringSubmatch(themeDpiStr)
+	if len(substr) < 2 {
+		logger.Warning("cannot parse theme dpi")
+		return
+	}
+	themeDpi, err := strconv.ParseUint(substr[1], 10, 32)
+	if err != nil {
+		logger.Warning(err)
+		return
+	}
+	scale, err := kf.GetFloat64(themeSection, themeKeyScreenScaleFactors)
+	xDpi := uint64(scale * float64(themeDpi) * 1024)
+	_, err = xf.WriteString(fmt.Sprintf("%s %d\n", xsettingsKeyDpi, xDpi))
+	if err != nil {
+		logger.Warning(err)
+	}
+}
+
 func (*Manager) GetInterfaceName() string {
 	return dbusInterface
+}
+
+func isInteger(str string) bool {
+	_, err := strconv.ParseInt(str, 10, 32)
+	return err == nil
 }
