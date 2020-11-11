1590802f550fc02b3f7b2d5286e3a1e54d84d651
electricface <songwentai@uniontech.com>
2020-11-11 14:26:00 +0800

    chore(power_manager): 给 canSuspend 补充一个待办
    增加 TODO 说明
    给 canHibernate 也加了一个
    
    Log: 给 canSuspend 补充一个待办
    Change-Id: I9db9e6d4aabfd8a3bb863ea0103f239c10de75a8

--- system/power_manager/utils.go
+++ system/power_manager/utils.go
@@ -13,6 +13,10 @@ const (
 )
 
 func canSuspend() bool {
+	// TODO: 这种判断方式只能作为当前的临时方案使用，不是一个标准的判断是否支持待机的方法，
+	// 等到内核对systemd的login中判断是否能待机的DBus接口(服务名 org.freedesktop.login1，
+	// 对象 /org/freedesktop/login1，接口 org.freedesktop.login1.Manager 方法 CanSuspend)
+	// 支持完善以后就要移除这部分逻辑。
 	data, err := ioutil.ReadFile(fileMemSleep)
 	if err != nil {
 		logger.Warningf("read %s failed: %v", fileMemSleep, err)
@@ -27,6 +31,7 @@ func canSuspend() bool {
 }
 
 func canHibernate() bool {
+	// TODO: 需要考虑是否需要这些对于内存和swap的判断。
 	data, err := ioutil.ReadFile(fileImageSize)
 	if err != nil {
 		logger.Warningf("read %s failed: %v", fileImageSize, err)
