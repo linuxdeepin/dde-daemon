bb1476373c6e3ae6be3b7d7ce9ffbe1d7704d9f8
ut001015 <yangbiao@uniontech.com>
2020-12-16 14:58:21 +0800

    fix(bluetooth): 连接蓝牙耳机后再次连接辅助PC,耳机链接断开
    原因是pc被当做蓝牙声卡设备识别了，就会断开之前链接的设备
    
    Log: 增加链接类型的判断，是audio-card类型才做断开动作
    Task: https://pms.uniontech.com/zentao/bug-view-57653.html
    Change-Id: Ic27e4d8ac5b06ed3a2b6beb7cfc71a16eeefda9c

--- bluetooth/a2dp_workaround.go
+++ bluetooth/a2dp_workaround.go
@@ -27,8 +27,11 @@ func (b *Bluetooth) disconnectA2DPDeviceExcept(d *device) {
 			}
 			for _, uuid := range device.UUIDs {
 				if uuid == A2DP_SINK_UUID {
-					logger.Infof("disconnect A2DP %s", device)
-					device.Disconnect()
+					if d.Icon == DeviceTypes[AudioCard]{
+						logger.Infof("disconnect A2DP %s", device)
+						device.Disconnect()
+					}
+					
 				}
 			}
 		}
