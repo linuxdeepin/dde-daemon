0a02bd85a7dd8d85b95b89dbf4274d589c02975c
longqi <longqi@uniontech.com>
2021-01-07 16:49:56 +0800

    fix: fix system/power test error on i386
    解决单元测试中，没有考虑到32位系统的问题
    
    Log: 解决单元测试问题
    
    Change-Id: I842de292950c755b882a87a44ba66a7ccc49561e

--- system/power/lid_switch_common_test.go
+++ system/power/lid_switch_common_test.go
@@ -1,36 +1,31 @@
 package power
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_NBITS(t *testing.T) {
-	var data = 64
+	var data = strconv.IntSize
 	assert.Equal(t, NBITS(data), 1)
-	data = 65
+	data = strconv.IntSize + 1
 	assert.Equal(t, NBITS(data), 2)
 }
 
 func Test_LONG(t *testing.T) {
 	var data = 1
 	assert.Equal(t, LONG(data), 0)
-	data = 65
+	data = strconv.IntSize + 1
 	assert.Equal(t, LONG(data), 1)
-	data = 128
+	data = strconv.IntSize * 2
 	assert.Equal(t, LONG(data), 2)
 }
 
 func Test_OFF(t *testing.T) {
 	var data = 1
 	assert.Equal(t, OFF(data), 1)
-	data = 5
-	assert.Equal(t, OFF(data), 5)
-}
-
-func Test_testBit(t *testing.T) {
-	data := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	assert.Equal(t, testBit(5, data), false)
-	assert.Equal(t, testBit(64, data), true)
+	data = 33
+	assert.Equal(t, OFF(data), data%strconv.IntSize)
 }
