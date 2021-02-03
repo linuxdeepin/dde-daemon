b81abad76dd06d2d53e09c20a05c935d3ffa5513
dengbo <dengbo@uniontech.com>
2021-02-03 15:51:30 +0800

    chore: 代码优化
    参考Session DBus接口的实现增加System DBus接口中对设置时域的有效性判断，并删除了多余的注释代码
    
    Log: 代码优化
    Change-Id: I4456582e3f9586294a3594808227c90681278eb5

--- system/timedated/manager_ifc.go
+++ system/timedated/manager_ifc.go
@@ -23,19 +23,13 @@ import (
 	"os"
 
 	"github.com/godbus/dbus"
+	"pkg.deepin.io/dde/daemon/timedate/zoneinfo"
 	"pkg.deepin.io/lib/dbusutil"
 )
 
 // SetTime set the current time and date,
 // pass a value of microseconds since 1 Jan 1970 UTC
 func (m *Manager) SetTime(sender dbus.Sender, usec int64, relative bool, msg string) *dbus.Error {
-	/*
-		err := m.checkAuthorization("SetTime", msg, sender)
-		if err != nil {
-			return dbusutil.ToError(err)
-		}
-	*/
-
 	// TODO: check usec validity
 	err := m.core.SetTime(0, usec, relative, false)
 	return dbusutil.ToError(err)
@@ -43,12 +37,16 @@ func (m *Manager) SetTime(sender dbus.Sender, usec int64, relative bool, msg str
 
 // SetTimezone set the system time zone, the value from /usr/share/zoneinfo/zone.tab
 func (m *Manager) SetTimezone(sender dbus.Sender, timezone, msg string) *dbus.Error {
+	if !zoneinfo.IsZoneValid(timezone) {
+		logger.Warning("invalid zone:", timezone)
+		return dbusutil.ToError(zoneinfo.ErrZoneInvalid)
+	}
+
 	err := m.checkAuthorization("SetTimezone", msg, sender)
 	if err != nil {
 		return dbusutil.ToError(err)
 	}
 
-	// TODO: check timezone validity
 	currentTimezone, err := m.core.Timezone().Get(0)
 	if err != nil {
 		return dbusutil.ToError(err)
@@ -82,13 +80,6 @@ func (m *Manager) SetLocalRTC(sender dbus.Sender, enabled bool, fixSystem bool, 
 
 // SetNTP to control whether the system clock is synchronized with the network
 func (m *Manager) SetNTP(sender dbus.Sender, enabled bool, msg string) *dbus.Error {
-	/*
-		err := m.checkAuthorization("SetNTP", msg, sender)
-		if err != nil {
-			return dbusutil.ToError(err)
-		}
-	*/
-
 	currentNTPEnabled, err := m.core.NTP().Get(0)
 	if err != nil {
 		return dbusutil.ToError(err)
