a9d8e1ad53806afa1f6cd5055b797852df465e7a
weizhixiang <weizhixiang@uniontech.com>
2021-03-09 11:45:36 +0800

    fix(network): WiFi设置中先保存密码，再点击WiFi连接，仍然弹出密码认证框
    保存WiFi密码设置后直接存进keyring, 取消getSecrets拦截密码，通过requestNew字段判断是否需要弹输入密码窗
    
    Log: WiFi设置中先保存密码后，再点击WiFi连接，仍然弹出密码认证框
    Bug: https://pms.uniontech.com/zentao/bug-view-52125.html
    Change-Id: I36aeedbff5848e8ce9d637527767891991543550

--- network/manager_connection.go
+++ network/manager_connection.go
@@ -454,8 +454,6 @@ func (m *Manager) activateConnection(uuid string, devPath dbus.ObjectPath) (cpat
 		}
 	}
 
-	m.hasSaveSecret = true
-	m.saveToKeyring = true
 	_, err = nmActivateConnection(cpath, devPath)
 	return
 }
