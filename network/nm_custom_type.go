cc0e704fa8883ddbdc254caaa3275bb2eebba4b1
Hermit Huang <hehuang.ustc@gmail.com>
2021-04-19 13:56:56 +0800

    fix: add fix device type for linuxdeepin/dde-daemon#109
    add the missing device type defined in nm-dbus-types
    
    Log:

--- network/nm_custom_type.go
+++ network/nm_custom_type.go
@@ -43,6 +43,7 @@ const (
 	deviceGeneric    = "generic"
 	deviceTeam       = "team"
 	deviceTun        = "tun"
+	deviceWifiP2p    = "wifip2p"
 )
 
 func getCustomDeviceType(devType uint32) (customDevType string) {
@@ -79,6 +80,8 @@ func getCustomDeviceType(devType uint32) (customDevType string) {
 		return deviceTeam
 	case nm.NM_DEVICE_TYPE_TUN:
 		return deviceTun
+	case nm.NM_DEVICE_TYPE_WIFI_P2P:
+		return deviceWifiP2p
 	case nm.NM_DEVICE_TYPE_UNKNOWN:
 	default:
 		logger.Error("unknown device type", devType)
