af8dea066d2848d8e00fbeff60b1437696c3c871
electricface <songwentai@uniontech.com>
2021-05-28 20:50:51 +0800

    fix(dock): 任务栏日历鼠标悬浮窗显示为“深度日历”
    调用了错误的方法，应该调用 entry.updateName()
    
    Log: 修复任务栏日历鼠标悬浮窗显示为“深度日历”
    Bug: https://pms.uniontech.com/zentao/bug-view-108644.html
    Influence: 任务栏插件显示/使用
    Change-Id: Ie65eb47fde6b2a980b53a18b812c49c5ab607d07

--- dock/dock_manager_init.go
+++ dock/dock_manager_init.go
@@ -140,7 +140,7 @@ func (m *Manager) handleLauncherItemCreated(itemInfo launcher.ItemInfo) {
 	}
 }
 
-//在收到应用更新的信号后，如果app对desktop文件的'Exec'属性做了更改，需要更新该应用之前的'appInfo'和'innerId'为新的值
+// 在收到 launcher item 更新的信号后，需要更新相关信息，包括 appInfo、innerId、名称、图标、菜单。
 func (m *Manager) handleLauncherItemUpdated(itemInfo launcher.ItemInfo) {
 	desktopFile := toLocalPath(itemInfo.Path)
 	entry, err := m.Entries.GetByDesktopFilePath(desktopFile)
@@ -158,6 +158,9 @@ func (m *Manager) handleLauncherItemUpdated(itemInfo launcher.ItemInfo) {
 	}
 	entry.setAppInfo(appInfo)
 	entry.innerId = appInfo.innerId
+	entry.updateName()
+	entry.updateMenu()
+	entry.updateIcon()
 }
 
 func (m *Manager) listenLauncherSignal() {
