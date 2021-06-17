959652a118cebf121d0eede9964d97f929696568
lichangze <lichangze@uniontech.com>
2021-06-17 19:48:35 +0800

    fix: 修复删除网络账户时,/home目录下内容未删除的问题
    修复删除网络账户时,/home目录下修复删除网络账户时,/home目录下内容未删除的问题
    内容未删除的问题
    
    Log: 修复删除网络账户时,/home目录下内容未删除的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-84099.html
    Influence: 网络账户模块
    Change-Id: I0114dcc64748db448ed72bf1eb266f0600119554

--- accounts/manager_ifc.go
+++ accounts/manager_ifc.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
 	dbus "github.com/godbus/dbus"
@@ -152,6 +153,11 @@ func (m *Manager) DeleteUser(sender dbus.Sender,
 		//delete user config and icons
 		if rmFiles {
 			user.clearData()
+			err := os.RemoveAll(user.HomeDir)
+			if err != nil {
+				logger.Warning(err)
+				return dbusutil.ToError(err)
+			}
 		}
 
 		return nil
