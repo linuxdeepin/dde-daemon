1c8dfb395ba928c217190648f82299833ab5c5ab
weizhixiang <weizhixiang@uniontech.com>
2021-06-28 18:37:41 +0900

    fix(bluetooth): 修复重启后蓝牙不能自动连接的问题
    E人E本平板上蓝牙实现脚本错误，蓝牙地址在重启后改变，导致蓝牙配置在重启后重新初始化
    
    Log:
    Bug: https://pms.uniontech.com/zentao/bug-view-85316.html
    Change-Id: Ib5b4363280376cafdfc1ed4218310a5bd746dfc4

--- misc/dde-daemon/bluetooth/bluetooth_init.sh
+++ misc/dde-daemon/bluetooth/bluetooth_init.sh
@@ -7,7 +7,7 @@ sleep 2
 echo 1 > /sys/class/rfkill/rfkill0/state
 sleep 2
 
-brcm_patchram_plus1 --bd_addr_rand --enable_hci --no2bytes --use_baudrate_for_download  --tosleep  200000 --baudrate 1500000 --patchram  /system/etc/firmware/"BCM4356A2.hcd" /dev/ttyS0 &
+brcm_patchram_plus1 --enable_hci --no2bytes --use_baudrate_for_download  --tosleep  200000 --baudrate 1500000 --patchram  /system/etc/firmware/"BCM4356A2.hcd" /dev/ttyS0 &
 
 
 handlebluetooth() {
