486a00ca1e68120fba766e8eb2a7dc7b118c12a5
luokai <luokai@uniontech.com>
2021-07-21 10:14:47 +0800

    chore: 上传翻译文件
    上传翻译文件
    
    Log: 上传翻译文件
    Bug: https://pms.uniontech.com/zentao/bug-view-88173.html
    Change-Id: Ib9c1be7e3e477f932425fdd1dad64ef58281d9af

--- misc/ts/com.deepin.daemon.Grub2.policy/policy_ka.ts
+++ misc/ts/com.deepin.daemon.Grub2.policy/policy_ka.ts
@@ -1,25 +1,25 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="sl" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ka" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.daemon.Grub2!message" line="0"/>
 			<source>Authentication is required to change the grub2 configuration</source>
-			<translation>Za spreminjanje konfiguracije grub2 je potrebna avtentikacija</translation>
+			<translation>grub2 კონფიგურაციის შესაცვლელად საჭიროა ავტორიზაციის გავლა</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.Grub2!description" line="0"/>
 			<source>Change the grub2 configuration</source>
-			<translation>Spremenite konfiguracijo grub2</translation>
+			<translation>grub2 კონფიგურაციის შეცვლა</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.grub2.prepare-gfxmode-detect!message" line="0"/>
 			<source>Authentication is required to prepare grub2 display resolution detection</source>
-			<translation>Priprava grub2 zaznavanja ločljivosti zaslona zahteva overitev</translation>
+			<translation>grub2 ეკრანის გაფართოების შესაცვლელად საჭიროა ავტორიზაცია</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.grub2.prepare-gfxmode-detect!description" line="0"/>
 			<source>Prepare grub2 display resolution detection</source>
-			<translation>Pripravi grub2 zaznavanje ločljivosti zaslona</translation>
+			<translation>grub2 ეკრანის გაფართოების შეცვლა</translation>
 		</message>
 	</context>
 </TS>
