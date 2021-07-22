9aa051461f6aa50df8dd846ed53d9660b9a3f189
liaohanqin <liaohanqin@uniontech.com>
2021-07-22 14:52:26 +0800

    feat: IP冲突接口调整
    因需要设计变化，接口调整，检测函数不超时等待，通过信号反馈
    
    Log: IP冲突检测方式调整
    Task: https://pms.uniontech.com/zentao/task-view-78387.html
    Change-Id: I0054828d9d68f5f5738b2fd822cd534ab962f3cd
    (cherry picked from commit 5417028c7b9e165fe2931ee0676b5b76de5639c3)

--- network/manager_ip_conflict.go
+++ network/manager_ip_conflict.go
@@ -4,90 +4,41 @@ import (
 	"time"
 
 	dbus "github.com/godbus/dbus"
-	"pkg.deepin.io/lib/dbusutil"
+	ipwatchd "github.com/linuxdeepin/go-dbus-factory/com.deepin.system.ipwatchd"
 )
 
-const (
-	interfaceIPConflict  = "com.deepin.system.IPWatchD"
-	objectPathIPConflict = "/com/deepin/system/IPWatchD"
-	memberIPConflict     = "IPConflict"
-	signalNameIPConflict = "IPConflictCheck"
-)
+func (m *Manager) initIPConflictManager(sysBus *dbus.Conn) {
+	m.sysIPWatchD = ipwatchd.NewIPWatchD(sysBus)
+	m.sysIPWatchD.InitSignalExt(m.sysSigLoop, true)
 
-func (m *Manager) initIPConflictManager() {
-	err := dbusutil.NewMatchRuleBuilder().
-		Type("signal").
-		Interface(interfaceIPConflict).
-		Member(memberIPConflict).Build().
-		AddTo(m.sysSigLoop.Conn())
+	_, err := m.sysIPWatchD.ConnectIPConflict(func(ip, smac, dmac string) {
+		err := m.service.Emit(manager, "IPConflict", ip, dmac)
+		if err != nil {
+			logger.Warning(err)
+		}
+	})
 	if err != nil {
 		logger.Warning(err)
 	}
-
-	sysSigLoop.AddHandler(&dbusutil.SignalRule{
-		Name: interfaceIPConflict + "." + memberIPConflict,
-	}, func(sig *dbus.Signal) {
-		if len(sig.Body) == 3 {
-			ip, ok := sig.Body[0].(string)
-			if !ok {
-				logger.Warning("ip must string type")
-				return
-			}
-			macA, ok := sig.Body[1].(string)
-			if !ok {
-				logger.Warning("mac must string type")
-				return
-			}
-			macB, ok := sig.Body[2].(string)
-			if !ok {
-				logger.Warning("mac must string type")
-				return
-			}
-			if len(macA) != 0 && len(macB) != 0 {
-				err := m.service.Emit(m, memberIPConflict, ip, macB)
-				if err != nil {
-					logger.Debug(err)
-				}
-			}
-		}
-	})
 }
 
-func (m *Manager) RequestIPConflictCheck(ip, ifc string) (string, *dbus.Error) {
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		return "", dbusutil.ToError(err)
-	}
-
-	err = conn.Emit(objectPathIPConflict, interfaceIPConflict+"."+signalNameIPConflict, ip, ifc)
-	if err != nil {
-		return "", dbusutil.ToError(err)
-	}
-
-	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0,
-		"type='signal',path='/com/deepin/system/IPWatchD',interface='com.deepin.system.IPWatchD'")
-
-	c := make(chan *dbus.Signal, 10)
-	conn.Signal(c)
-
-	t := time.NewTimer(500 * time.Millisecond)
-	for {
+func (m *Manager) RequestIPConflictCheck(ip, ifc string) *dbus.Error {
+	ch := make(chan *dbus.Call, 1)
+	m.sysIPWatchD.GoRequestIPConflictCheck(0, ch, ip, ifc)
+	go func() {
 		select {
-		case <-t.C:
-			logger.Debug("wait ip conflict signal timeout")
-			return "", nil
-		case v := <-c:
-			if v.Name == "com.deepin.system.IPWatchD.IPConflict" && len(v.Body) == 3 {
-				if val, ok := v.Body[0].(string); ok && ip == val {
-					ret, ok := v.Body[2].(string)
-					if !ok {
-						logger.Warning("mac must string type")
-						continue
-					}
-
-					return ret, nil
-				}
+		case ret := <-ch:
+			mac := ""
+			err := ret.Store(&mac)
+			if err != nil {
+				logger.Warning(err)
+				return
 			}
+			m.service.Emit(manager, "IPConflict", ip, mac)
+		case <-time.After(1 * time.Second):
+			return
 		}
-	}
+	}()
+
+	return nil
 }
