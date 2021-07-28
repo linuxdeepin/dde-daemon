4afdad82c7fa3dd154b852a97bc5df2c973b7d31
liaohanqin <liaohanqin@uniontech.com>
2021-07-28 10:59:01 +0800

    fix(network): 启动IP冲突逻辑前，启动ipwatchd服务
    由于dbus启动的原因，可能导致ipwatchd未能启动，增加相关的启动逻辑
    
    Log: network启动前，启动IP冲突服务
    Bug: https://pms.uniontech.com/zentao/bug-view-88932.html
    Change-Id: Ib4511caf5385045e539036f31dfbf3b2b3366352

--- network/manager_ip_conflict.go
+++ network/manager_ip_conflict.go
@@ -5,13 +5,38 @@ import (
 
 	dbus "github.com/godbus/dbus"
 	ipwatchd "github.com/linuxdeepin/go-dbus-factory/com.deepin.system.ipwatchd"
+	ofdbus "github.com/linuxdeepin/go-dbus-factory/org.freedesktop.dbus"
 )
+
+func activateSystemService(sysBus *dbus.Conn, serviceName string) error {
+	sysBusObj := ofdbus.NewDBus(sysBus)
+
+	has, err := sysBusObj.NameHasOwner(0, serviceName)
+	if err != nil {
+		return err
+	}
+
+	if has {
+		logger.Debug("service activated", serviceName)
+		return nil
+	}
+	_, err = sysBusObj.StartServiceByName(0, serviceName, 0)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (m *Manager) initIPConflictManager(sysBus *dbus.Conn) {
 	m.sysIPWatchD = ipwatchd.NewIPWatchD(sysBus)
 	m.sysIPWatchD.InitSignalExt(m.sysSigLoop, true)
+	err := activateSystemService(sysBus, m.sysIPWatchD.ServiceName_())
+	if err != nil {
+		logger.Warning(err)
+	}
 
-	_, err := m.sysIPWatchD.ConnectIPConflict(func(ip, smac, dmac string) {
+	_, err = m.sysIPWatchD.ConnectIPConflict(func(ip, smac, dmac string) {
 		err := m.service.Emit(manager, "IPConflict", ip, dmac)
 		if err != nil {
 			logger.Warning(err)
