92d37179d1f0e1cbc86696d7dbc7ae13d91ca56b
tanfeng <tanfeng@uniontech.com>
2021-11-23 15:47:27 +0800

    fix: 修复新建一个帐户，切换到新帐户登录界面，有概率登录失败需要登录2次问题
    切换用户，lightdm创建新xorg，xorg会有当前hostname的授权信息，hostname变更后，xorg授权信息过时，导致startdde连接xorg失败。 修改hostname后需要重启xorg。
    
    Log: 修复修改hostname后导致切换用户登录异常的问题
    Influence: 登录界面（切换用户登录）
    Bug: https://pms.uniontech.com/zentao/bug-view-89473.html
    Change-Id: Ia8c664defade0dd2e7d07f6011d561cd0dcd5310

---
+++ system/hostname/hostname.go
@@ -0,0 +1,153 @@
+package hostname
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+
+	hostname1 "github.com/linuxdeepin/go-dbus-factory/org.freedesktop.hostname1"
+	login1 "github.com/linuxdeepin/go-dbus-factory/org.freedesktop.login1"
+	"pkg.deepin.io/dde/daemon/loader"
+	"pkg.deepin.io/lib/dbusutil"
+	"pkg.deepin.io/lib/log"
+)
+
+type Module struct {
+	*loader.ModuleBase
+	hostname *HostName
+}
+
+func (m *Module) GetDependencies() []string {
+	return nil
+}
+
+func (m *Module) Start() error {
+	if m.hostname != nil {
+		return nil
+	}
+	logger.Debug("start hostname service")
+	m.hostname = newHostName()
+
+	service := loader.GetService()
+	m.hostname.service = service
+
+	m.hostname.init()
+
+	return nil
+}
+
+func (m *Module) Stop() error {
+	return nil
+}
+
+var logger = log.NewLogger("daemon/system/hostname")
+
+func newModule(logger *log.Logger) *Module {
+	m := new(Module)
+	m.ModuleBase = loader.NewModuleBase("hostname", m, logger)
+	return m
+}
+
+func init() {
+	loader.Register(newModule(logger))
+}
+
+type HostName struct {
+	service *dbusutil.Service
+	sigLoop *dbusutil.SignalLoop
+}
+
+func newHostName() *HostName {
+	return &HostName{}
+}
+
+func (h *HostName) init() {
+	h.sigLoop = dbusutil.NewSignalLoop(h.service.Conn(), 10)
+	h.sigLoop.Start()
+
+	err := h.listenHostNameChangeSignals()
+	if err != nil {
+		logger.Warning("hostname listen error:", err)
+	}
+}
+
+func (h *HostName) listenHostNameChangeSignals() error {
+	hostnameObj := hostname1.NewHostname(h.service.Conn())
+	hostnameObj.InitSignalExt(h.sigLoop, true)
+	err := hostnameObj.Hostname().ConnectChanged(func(hasValue bool, value string) {
+		if !hasValue {
+			return
+		}
+		login := login1.NewManager(h.service.Conn())
+		sessions, err := login.ListSessions(0)
+		if err != nil {
+			logger.Warning("call login.ListSessions error:", err)
+			return
+		}
+		xorgCmdContent := ""
+		for _, session := range sessions {
+			logger.Debug("session:", session.Path)
+			if session.UserName == "lightdm" {
+				sessionObj, err := login1.NewSession(h.service.Conn(), session.Path)
+				if err != nil {
+					logger.Warning("call login1.NewSession error:", err)
+					continue
+				}
+				display, err := sessionObj.Display().Get(0)
+				if err != nil {
+					logger.Warning("call sessionObj.Display().Get(0) error:", err)
+					continue
+				}
+				if display != "" {
+					xorgCmdContent = "/var/run/lightdm/root/" + display
+					break
+				}
+			}
+		}
+		logger.Debug("xorgCmdContent:", xorgCmdContent)
+		if xorgCmdContent != "" {
+			dirList, err := ioutil.ReadDir("/proc/")
+			if err != nil {
+				logger.Debug("read dir /proc error:", err)
+				return
+			}
+			for _, dir := range dirList {
+				if !dir.IsDir() {
+					continue
+				}
+				procPath := filepath.Join("/proc/", dir.Name())
+				linkDst, err := os.Readlink(filepath.Join(procPath, "exe"))
+				if err != nil {
+					logger.Debug("Readlink error:", err)
+					continue
+				}
+				dstFileName := path.Base(linkDst)
+				if dstFileName != "Xorg" {
+					continue
+				}
+				cmdline := filepath.Join(procPath, "cmdline")
+				content, err := ioutil.ReadFile(cmdline) // #nosec G304
+				if err != nil {
+					logger.Debug("ReadFile error:", err)
+					continue
+				}
+				if !strings.Contains(string(content), xorgCmdContent) {
+					logger.Debug("cmdline not contains xorgCmdContent:", xorgCmdContent)
+					continue
+				}
+				pid := dir.Name()
+				err = exec.Command("kill", "-9", pid).Run() // #nosec G204
+				if err != nil {
+					logger.Warning("to kill process err:", err)
+				}
+				logger.Debug("to kill xorg process, pid:", pid)
+				break
+			}
+		}
+	})
+
+	return err
+}
