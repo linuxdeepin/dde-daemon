4b4341b363cd0a124673133e6497ad608f6bb32c
ut000168 <wubowen@uniontech.com>
2023-01-09 14:14:54 +0800

    feat: 白盒密码创建WB_UFile优化
    当string转[]byte时，如果string转成hex后存在00,byte就会直接被截断，导致数据不全
    
    Log: 优化WB_UFile创建
    Influence: 白盒密码创建WB_UFile
    Task: https://pms.uniontech.com/task-view-212895.html
    Change-Id: I3df1e5638e9573bddccaea53f258f9a61a0c7ae9

--- accounts/keyring/common.h
+++ accounts/keyring/common.h
@@ -12,9 +12,6 @@
 #include <locale.h>
 #include <pthread.h>
 #include <pwd.h>
-#include <security/_pam_types.h>
-#include <security/pam_ext.h>
-#include <security/pam_modules.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -29,15 +26,10 @@
 #include <sys/time.h>
 
 #define MASTER_KEY_LEN 16
-#define FILE_PATH_BUF_SIZE 128
-#define MAX_BUF_SIZE 256
-#define LIMITS_BUF_SIZE 1024
-
 #define ENCRYPT_MODE 1
 #define DECRYPT_MODE 0
 
 void *dalloc(size_t size);
-char *generate_random_str();
 char *generate_random_len(unsigned int length);
 unsigned char* deepin_wb_encrypt(unsigned char* IN, unsigned char* key, bool flag);
 unsigned char* sm4_crypt(unsigned char* IN, unsigned char* key, int mode);
