485287e3f2f8855463e0bac2eb6681b2c4ce5652
chenhongtao <chenhongtao@uniontech.com>
2023-04-07 20:45:17 +0800

    chore: add gen.go
    Log: add gen.go support
    
    (cherry picked from commit 44323fd4d6153c99aacfb09cbd8dbefb5dcc179f)

---
+++ gen.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package daemon
+
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/dde-session-daemon
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/dde-system-daemon
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/grub2
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/search
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/backlight_helper
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/langselector
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/soundeffect
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/dde-lockservice
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/dde-authority
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/default-terminal
+//go:generate go build -o target/ github.com/linuxdeepin/dde-daemon/bin/dde-greeter-setter
