f89b159ad20184cd5b6ada0f33f89506dc880b9c
rewine <luhongxu@deepin.org>
2023-05-08 14:06:56 +0800

    feat: search in XDG directories
    Log: search in XDG directories

--- gesture/config.go
+++ gesture/config.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
+	"github.com/adrg/xdg"
 	"github.com/linuxdeepin/go-lib/xdg/basedir"
 )
 
@@ -21,11 +22,10 @@ const (
 
 var (
 	configUserPath = filepath.Join(basedir.GetUserConfigDir(), "deepin/dde-daemon/gesture.json")
+	configSystemPath, _ = xdg.SearchDataFile("dde-daemon/gesture.json")
 )
 
 const (
-	configSystemPath = "/usr/share/dde-daemon/gesture.json"
-
 	gestureSchemaId         = "com.deepin.dde.gesture"
 	gsKeyTouchPadEnabled    = "touch-pad-enabled"
 	gsKeyTouchScreenEnabled = "touch-screen-enabled"
