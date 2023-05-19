609fafd2a22b0514bcf2b9dc767f899c11c49418
lichangze <lichangze@uniontech.com>
2023-05-19 10:41:29 +0800

    fix: grub2 获取os num的忽略标题修改
    Uniontech OS -> UOS
    
    Log: grub2 获取os num的忽略标题修改
    Task: https://pms.uniontech.com/task-view-267459.html
    Influence: grub get os num
    Change-Id: I1f0838c8df8e3a6412ac6a3b7329397cbd894c7c

--- grub2/grub2.go
+++ grub2/grub2.go
@@ -578,7 +578,7 @@ func getGfxmodesFromBootArg() (grub_common.Gfxmodes, error) {
 	return allGfxmodes, nil
 }
 
-var ignoreString = []string{"System setup", "UnionTech OS Restore"}
+var ignoreString = []string{"System setup", "UOS Restore"}
 
 func getOSNum(entries []Entry) uint32 {
 	var systemNum uint32
