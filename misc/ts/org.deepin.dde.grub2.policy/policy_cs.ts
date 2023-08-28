111ccc6f32ccc097d92b7e83e7c4a8bf62e3f92c
chenhongtao <chenhongtao@deepin.org>
2023-08-28 09:40:47 +0800

    chore: update translations
    Log:

--- misc/ts/org.deepin.dde.grub2.policy/policy_cs.ts
+++ misc/ts/org.deepin.dde.grub2.policy/policy_cs.ts
@@ -14,7 +14,7 @@
 		<message>
 			<location filename="org.deepin.dde.grub2.prepare-gfxmode-detect!message" line="0"/>
 			<source>Authentication is required to prepare grub2 display resolution detection</source>
-			<translation>Pro zjištění rozlišení obrazovky grub2 je požadováno ověření se</translation>
+			<translation>Pro přípravu zjišťování rozlišení obrazovky grub2 je požadováno ověření se</translation>
 		</message>
 		<message>
 			<location filename="org.deepin.dde.grub2.prepare-gfxmode-detect!description" line="0"/>
