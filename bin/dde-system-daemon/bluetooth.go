4f3d8203b4246207d50c53ca9a931a913c978fc6
liaohanqin <liaohanqin@uniontech.com>
2023-08-28 10:41:54 +0800

    fix: 修复蓝牙漏洞
    增加安全判断，防止用户跨目录。
    
    Log: 修复蓝牙漏洞
    Task: https://pms.uniontech.com/task-view-288093.html
    Influence: 无
    Change-Id: I547d25904d258ba2196cfa18d45991ef66b834a8
    (cherry picked from commit 99ea56aa668075ed62fe79a6b506198a459033ad)

--- bin/dde-system-daemon/bluetooth.go
+++ bin/dde-system-daemon/bluetooth.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"fmt"
 	"path/filepath"
 
 	dbus "github.com/godbus/dbus"
@@ -21,6 +22,9 @@ const (
 
 func (*Daemon) BluetoothGetDeviceTechnologies(adapter, device string) (technologies []string, busErr *dbus.Error) {
 	var filename = filepath.Join(bluetoothPrefixDir, adapter, device, "info")
+	if !filepath.HasPrefix(filename, bluetoothPrefixDir) {
+		return nil, dbusutil.ToError(fmt.Errorf("invaild adapter: [%s] device: [%s]", adapter, device))
+	}
 	technologies, err := doBluetoothGetDeviceTechnologies(filename)
 	if err != nil {
 		return nil, dbusutil.ToError(err)
@@ -32,7 +36,7 @@ func doBluetoothGetDeviceTechnologies(filename string) ([]string, error) {
 	var kf = keyfile.NewKeyFile()
 	err := kf.LoadFromFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("can't parse file: %s", filename)
 	}
 	return kf.GetStringList(kfSectionGeneral, kfKeyTechnologies)
 }
