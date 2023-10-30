3df1bc0774b020ad9830744e80f0501a28dd34dd
rewine <luhongxu@deepin.org>
2023-10-30 09:44:20 +0800

    fix: fix build with ddcutil 2.0
    fix: build

--- bin/backlight_helper/ddcci/ddcci.go
+++ bin/backlight_helper/ddcci/ddcci.go
@@ -103,11 +103,6 @@ func newDDCCI() (*ddcci, error) {
 		displayHandleMap: make(map[string]*displayHandle),
 	}
 
-	status := C.ddca_set_max_tries(C.DDCA_MULTI_PART_TRIES, 5)
-	if status < C.int(0) {
-		return nil, fmt.Errorf("brightness: Error setting retries: %d", status)
-	}
-
 	err := ddc.RefreshDisplays()
 	if err != nil {
 		return nil, err
