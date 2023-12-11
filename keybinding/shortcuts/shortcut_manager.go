864e220015e43e9d0ab5859ea3b91723c4b88a5e
chenjun <chenjun@uniontech.com>
2023-12-11 11:26:23 +0800

    fix: wyaland下不需要注册数字和大小写切换快捷键
    wyaland下不需要注册数字和大小写切换快捷键,会导致云桌面数字和大小写状态无法同步
    
    Log: 修复深信服云应用虚拟化环境切换大小写不同步问题
    Bug: https://pms.uniontech.com/bug-view-232587.html
    Influence: 切换数字键盘和大小写时正常显示OSD，并且云桌面状态同步
    Change-Id: I420c0cf4fb9aa58cc3e2d41997e63a0e0a100bdf

--- keybinding/shortcuts/shortcut_manager.go
+++ keybinding/shortcuts/shortcut_manager.go
@@ -86,8 +86,6 @@ func GetQtKeycodeMap() map[string]string {
 		"app-left":            "Qt::Key_ApplicationLeft",       // XF86ApplicationLeft
 		"app-right":           "Qt::Key_ApplicationRight",      // XF86ApplicationRight
 		"meeting":             "Qt::Key_Meeting",               // XF86Meeting
-		"numlock":             "Qt::Key_NumLock",
-		"capslock":            "Qt::Key_CapsLock",
 	}
 	return qtKeycodMap
 }
