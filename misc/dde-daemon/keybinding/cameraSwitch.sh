43dbd0fc2f1528be9eccbac57d9237f238ba58df
wubowen <wubowen@uniontech.com>
2023-12-19 13:31:49 +0800

    chore: update cameraSwitch.sh echo data
    更新开关相机提示
    
    Log: 更新开关相机提示
    Influence: 提示
    Change-Id: I19100f81e31871a25b4a7c303cca9680abf220ab
    (cherry picked from commit c0280cd35dfd232f26312384ed95a26fd685518f)

--- misc/dde-daemon/keybinding/cameraSwitch.sh
+++ misc/dde-daemon/keybinding/cameraSwitch.sh
@@ -4,10 +4,10 @@ count=$(ps -ef | grep deepin-camera | grep -w -c $USER)
 echo $count
 if [ $count -ge 2 ]
 then
-	echo "open deepin-camera"
+	echo "close deepin-camera"
 	killall deepin-camera
 else
-	echo "close deepin-camera"
+	echo "open deepin-camera"
 	dbus-send --session --print-reply --dest=com.deepin.SessionManager /com/deepin/StartManager com.deepin.StartManager.Launch string:/usr/share/applications/deepin-camera.desktop
 fi
 
