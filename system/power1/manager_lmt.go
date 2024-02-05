056c865011a3681ee54c7c4ee2e07c9f8360df0a
chenhongtao <chenhongtao@deepin.org>
2024-02-05 16:10:03 +0800

    chore(power)!: remove lmt logic prepare for tlp
    Log:

--- system/power1/manager_lmt.go
+++ system/power1/manager_lmt.go
@@ -5,85 +5,14 @@
 package power
 
 import (
-	"bufio"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 
 	dbus "github.com/godbus/dbus/v5"
 	"github.com/linuxdeepin/go-lib/dbusutil"
 )
 
-// laptop mode tools config file
-const lmtConfigFile = "/etc/laptop-mode/laptop-mode.conf"
-const laptopModeBin = "/usr/sbin/laptop_mode"
-
-const (
-	lmtConfigAuto     = 1
-	lmtConfigEnabled  = 2
-	lmtConfigDisabled = 3
-)
 
 const lowBatteryThreshold = 20.0
-
-func isLaptopModeBinOk() bool {
-	_, err := os.Stat(laptopModeBin)
-	return err == nil
-}
-
-func setLMTConfig(mode int) (changed bool, err error) {
-	lines, err := loadLmtConfig()
-	if err != nil {
-		// ignore not exist error
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-		logger.Warning(err)
-		return false, err
-	}
-
-	dict := make(map[string]string)
-	switch mode {
-	case lmtConfigAuto:
-		dict["ENABLE_LAPTOP_MODE_TOOLS"] = "1"
-		dict["ENABLE_LAPTOP_MODE_ON_BATTERY"] = "1"
-		dict["ENABLE_LAPTOP_MODE_ON_AC"] = "0"
-	case lmtConfigEnabled:
-		dict["ENABLE_LAPTOP_MODE_TOOLS"] = "1"
-		dict["ENABLE_LAPTOP_MODE_ON_BATTERY"] = "1"
-		dict["ENABLE_LAPTOP_MODE_ON_AC"] = "1"
-	case lmtConfigDisabled:
-		dict["ENABLE_LAPTOP_MODE_TOOLS"] = "1"
-		dict["ENABLE_LAPTOP_MODE_ON_BATTERY"] = "0"
-		dict["ENABLE_LAPTOP_MODE_ON_AC"] = "0"
-	}
-	lines, changed = modifyLMTConfig(lines, dict)
-	if changed {
-		logger.Debug("write LMT Config")
-		err = writeLmtConfig(lines)
-		if err != nil {
-			return false, err
-		}
-	}
-
-	return changed, nil
-}
-
-func reloadLaptopModeService() error {
-	if !isLaptopModeBinOk() {
-		logger.Debug("laptop mode tools is not installed")
-		return nil
-	}
-
-	systemBus, err := dbus.SystemBus()
-	if err != nil {
-		return err
-	}
-	systemdObj := systemBus.Object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
-	return systemdObj.Call("org.freedesktop.systemd1.Manager.ReloadUnit",
-		dbus.FlagNoAutoStart, "laptop-mode.service", "replace").Err
-}
 
 func modifyLMTConfig(lines []string, dict map[string]string) ([]string, bool) {
 	var changed bool
@@ -113,97 +42,18 @@ func modifyLMTConfig(lines []string, dict map[string]string) ([]string, bool) {
 	return lines, changed
 }
 
-func loadLmtConfig() ([]string, error) {
-	f, err := os.Open(lmtConfigFile)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(bufio.NewReader(f))
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
-	if scanner.Err() != nil {
-		return nil, scanner.Err()
-	}
-
-	return lines, nil
-}
-
-func writeLmtConfig(lines []string) error {
-	tempFile, err := writeLmtConfigTemp(lines)
-	if err != nil {
-		if tempFile != "" {
-			os.Remove(tempFile)
-		}
-		return err
-	}
-	return os.Rename(tempFile, lmtConfigFile)
-}
-
-func writeLmtConfigTemp(lines []string) (string, error) {
-	dir := filepath.Dir(lmtConfigFile)
-	err := os.MkdirAll(dir, 0755)
-	if err != nil {
-		return "", err
-	}
-
-	f, err := ioutil.TempFile(dir, "laptop-mode.conf")
-	logger.Debug("writeLmtConfig temp file", f.Name())
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	err = f.Chmod(0644)
-	if err != nil {
-		return f.Name(), err
-	}
-
-	bufWriter := bufio.NewWriter(f)
-	for _, line := range lines {
-		_, err := bufWriter.WriteString(line)
-		if err != nil {
-			logger.Warning(err)
-		}
-		err = bufWriter.WriteByte('\n')
-		if err != nil {
-			logger.Warning(err)
-		}
-	}
-	return f.Name(), bufWriter.Flush()
-}
 
 func (m *Manager) writePowerSavingModeEnabledCb(write *dbusutil.PropertyWrite) *dbus.Error {
 	logger.Debug("set laptop mode enabled", write.Value)
-
-	enabled := write.Value.(bool)
-	var err error
-	var lmtCfgChanged bool
 
 	m.PropsMu.Lock()
 	m.setPropPowerSavingModeAuto(false)
 	m.setPropPowerSavingModeAutoWhenBatteryLow(false)
 	m.PropsMu.Unlock()
 
-	if enabled {
-		lmtCfgChanged, err = setLMTConfig(lmtConfigEnabled)
-	} else {
-		lmtCfgChanged, err = setLMTConfig(lmtConfigDisabled)
-	}
 
-	if err != nil {
-		logger.Warning("failed to set LMT config:", err)
-	}
+	// TODO: add tlp part
 
-	if lmtCfgChanged {
-		err := reloadLaptopModeService()
-		if err != nil {
-			logger.Warning(err)
-		}
-	}
 
 	return nil
 }
@@ -214,7 +64,6 @@ func (m *Manager) updatePowerSavingMode() { // 根据用户设置以及当前状
 		return
 	}
 	var enable bool
-	var lmtCfgChanged bool
 	var err error
 	if !m.IsPowerSaveSupported {
 		enable = false
@@ -257,25 +106,8 @@ func (m *Manager) updatePowerSavingMode() { // 根据用户设置以及当前状
 
 	logger.Info("updatePowerSavingMode PowerSavingModeEnabled: ", enable)
 	m.PropsMu.Lock()
-	changed := m.setPropPowerSavingModeEnabled(enable)
+	m.setPropPowerSavingModeEnabled(enable)
 	m.PropsMu.Unlock()
-	if changed {
-		if enable {
-			lmtCfgChanged, err = setLMTConfig(lmtConfigEnabled)
-			if err != nil {
-				logger.Warning("failed to set LMT config:", err)
-			}
-		} else {
-			lmtCfgChanged, err = setLMTConfig(lmtConfigDisabled)
-			if err != nil {
-				logger.Warning("failed to set LMT config:", err)
-			}
-		}
-		if lmtCfgChanged {
-			err := reloadLaptopModeService()
-			if err != nil {
-				logger.Warning(err)
-			}
-		}
-	}
+	
+	// TODO: add tlp mode part
 }
