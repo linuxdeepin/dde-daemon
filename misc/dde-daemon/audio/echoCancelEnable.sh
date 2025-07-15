0233a358245f046b1090eb1e28e1f17644b27c59
fuleyi <fuleyi@uniontech.com>
2025-07-15 16:50:59 +0800

    fix: fix failure to change audio input device
    In ReduceNoise mode, dde audio can not get the master device on echo-cancel-source.
    
    Log:
    pms: BUG-324445

--- misc/dde-daemon/audio/echoCancelEnable.sh
+++ misc/dde-daemon/audio/echoCancelEnable.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-aecArgs="$*"
-# If no "aec_args" are passed on to the script, use this "aec_args" as default:
-[ -z "$aecArgs" ] && aecArgs="analog_gain_control=0 digital_gain_control=1"
+
+# 如果没有传递 aecArgs，使用默认值
 newSourceName="echo-cancel-source"
+aecArgs="analog_gain_control=0 digital_gain_control=1"
+sourceMaster=""
+
+# 解析参数
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --aec_args=*)
+            aecArgs="${1#*=}"
+            shift
+            ;;
+        --source_master=*)
+            sourceMaster="${1#*=}"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+moduleArgs="source_name=$newSourceName"
+if [ -n "$sourceMaster" ]; then
+    moduleArgs="$moduleArgs source_master=$sourceMaster"
+fi
 
 # Reload "module-echo-cancel"
-echo Reload \"module-echo-cancel\" with \"aec_args=$aecArgs\"
+echo Reload \"module-echo-cancel\" with \"aec_args=$aecArgs\" $moduleArgs
 pactl unload-module module-echo-cancel 2>/dev/null
-if pactl load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args=\"$aecArgs\" source_name=$newSourceName; then
-	# Set a new default source and sink, if module-echo-cancel has loaded successfully.
-	pactl set-default-source $newSourceName
+
+if pactl load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args=\"$aecArgs\" $moduleArgs; then
+    pactl set-default-source $newSourceName
 fi
